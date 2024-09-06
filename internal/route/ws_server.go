0dfabff9fab2d48651371e749908d6f53c6ad84d
hongyi <develop@yifeistudio.com>
2024-09-06 16:29:58 +0800

    建立webrtc连接

--- internal/route/ws_server.go
+++ internal/route/ws_server.go
@@ -3,24 +3,29 @@ package route
 import (
 	. "canoe/internal/model"
 	"encoding/json"
+	"fmt"
 	grl "github.com/gorilla/websocket"
 	"github.com/kataras/iris/v12/websocket"
 	"github.com/kataras/neffos"
 	"github.com/kataras/neffos/gorilla"
 	"github.com/pion/webrtc/v4"
 	"net/http"
+	"sync"
 )
 
-var w = webrtc.NewAPI(webrtc.WithSettingEngine(webrtc.SettingEngine{
-	// 配置 WebRTC 的设置
-}))
+var (
+	w = webrtc.NewAPI(webrtc.WithSettingEngine(webrtc.SettingEngine{
+		// 配置 WebRTC 的设置
+	}))
+	peers = make(map[string]*webrtc.PeerConnection)
+	mu    sync.Mutex
+)
 
-var peers = make(map[string]*webrtc.PeerConnection)
-
+// SignalingMessage 信令消息
 type SignalingMessage struct {
-	Type      string `json:"type"`
-	SDP       string `json:"sdp,omitempty"`
-	Candidate string `json:"candidate,omitempty"`
+	Type      string                  `json:"type"`
+	SDP       string                  `json:"sdp,omitempty"`
+	Candidate webrtc.ICECandidateInit `json:"candidate,omitempty"`
 }
 
 func handleLiveMsg(conn *neffos.NSConn, message neffos.Message) error {
@@ -48,7 +53,7 @@ func handleLiveMsg(conn *neffos.NSConn, message neffos.Message) error {
 			return err
 		}
 		answerMsg := SignalingMessage{
-			Type: "answer",
+			Type: webrtc.SDPTypeAnswer.String(),
 			SDP:  pc.LocalDescription().SDP,
 		}
 		resp, err := json.Marshal(answerMsg)
@@ -56,12 +61,55 @@ func handleLiveMsg(conn *neffos.NSConn, message neffos.Message) error {
 			return err
 		}
 		pc.OnTrack(func(track *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) {
-			println("handle ontrack message")
+			fmt.Printf("handle track message: %s \n", track.Codec().MimeType)
+			localTrack, err2 := webrtc.NewTrackLocalStaticRTP(track.Codec().RTPCodecCapability, track.ID(), track.StreamID())
+			if err2 != nil {
+				fmt.Printf("get track err: %s", err2)
+			}
+			rtpSender, err2 := pc.AddTrack(localTrack)
+			if err2 != nil {
+				fmt.Println("add track error:", err2)
+			}
+			go func() {
+				for {
+					rtpPacket, _, err := track.ReadRTP()
+					if err != nil {
+						fmt.Printf("handle error: %s\n", err)
+					}
+					err = localTrack.WriteRTP(rtpPacket)
+					if err != nil {
+						fmt.Printf("write error: %s\n", err)
+					}
+				}
+			}()
+			defer func(rtpSender *webrtc.RTPSender) {
+				err := rtpSender.Stop()
+				if err != nil {
+					fmt.Printf("stop err: %s\n", err)
+				}
+			}(rtpSender)
+		})
+		pc.OnConnectionStateChange(func(state webrtc.PeerConnectionState) {
+			fmt.Println("ICE Connection State has changed. ", state.String())
+		})
+		pc.OnICECandidate(func(candidate *webrtc.ICECandidate) {
+			if candidate == nil {
+				return
+			}
+			mu.Lock()
+			defer mu.Unlock()
+			candidateJson := candidate.ToJSON()
+			candidateMsg := SignalingMessage{
+				Type:      "candidate",
+				Candidate: candidateJson,
+			}
+			candidateStr, _ := json.Marshal(candidateMsg)
+			conn.Conn.Write(conn.Conn.DeserializeMessage(neffos.TextMessage, candidateStr))
 		})
 		conn.Conn.Write(conn.Conn.DeserializeMessage(neffos.TextMessage, resp))
 	case "candidate":
 		if pc, ok := peers["client"]; ok {
-			candidate := webrtc.ICECandidateInit{Candidate: msg.Candidate}
+			candidate := webrtc.ICECandidateInit{Candidate: msg.Candidate.Candidate}
 			if err := pc.AddICECandidate(candidate); err != nil {
 				return err
 			}
@@ -70,6 +118,7 @@ func handleLiveMsg(conn *neffos.NSConn, message neffos.Message) error {
 	return nil
 }
 
+// 处理普通聊天消息
 func handleChatMsg(conn *neffos.NSConn, message neffos.Message) error {
 	var evp Envelope
 	err := message.Unmarshal(&evp)
@@ -85,6 +134,7 @@ func handleChatMsg(conn *neffos.NSConn, message neffos.Message) error {
 	return nil
 }
 
+// 处理websocket 连接
 func wsServer(accessToken string, handler neffos.MessageHandlerFunc) *neffos.Server {
 	upgrader := gorilla.Upgrader(grl.Upgrader{
 		CheckOrigin: func(r *http.Request) bool { return true },
